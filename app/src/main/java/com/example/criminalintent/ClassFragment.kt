d459527043696fe7ffc295062acd579d467d2016
alyavit <alyavit1@gmail.com>
2024-11-19 19:38:39 +0700

    Добавление работы кнопок, чекбокса

--- app/src/main/java/com/example/criminalintent/ClassFragment.kt
+++ app/src/main/java/com/example/criminalintent/ClassFragment.kt
@@ -1,15 +1,21 @@
 package com.example.criminalintent
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 class ClassFragment : Fragment() {
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
+    private lateinit var dateButton: Button
+    private lateinit var solvedCheckBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +25,33 @@ class ClassFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_crime, container, false)
         titleField = view.findViewById<EditText>(R.id.crime_title)!!
-                // был view.findViewById(R.id.crime_title) as EditText
+            // был view.findViewById(R.id.crime_title) as EditText
+        dateButton = view.findViewById<Button>(R.id.crime_date)!!
+            //view.findViewById(R.id.crime_date) as Button
+        dateButton.apply {text = crime.date.toString()
+            isEnabled = false
+        }
+        solvedCheckBox = view.findViewById<CheckBox>(R.id.crime_solved)!!
+            //view.findViewById(R.id.crime_solved) as CheckBox
         return view
     }
+
+    override fun onStart() {
+        super.onStart()
+        val titleWatcher = object : TextWatcher
+        {
+            override fun beforeTextChanged(sequence: CharSequence?, start: Int, count: Int, after: Int) {
+            // Это пространство оставлено пустым специально
+            }
+
+            override fun onTextChanged(sequence: CharSequence?,start: Int,before: Int,count: Int){
+                crime.title
+                sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+            // И это
+            }
+        }
+        titleField.addTextChangedListener(titleWatcher)}
 }
